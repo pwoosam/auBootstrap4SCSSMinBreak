c916257e6e0e07bb61662d194167cc36cad74e96
Patrick Woo-Sam <pwoosam5@icloud.com>
2017-12-14 23:31:25 -0800

    Fixed by using node-ruby-sass

--- aurelia_project/tasks/process-css.ts
+++ aurelia_project/tasks/process-css.ts
@@ -1,13 +1,13 @@
 import * as gulp from 'gulp';
 import * as sourcemaps from 'gulp-sourcemaps';
-import * as sass from 'gulp-sass';
+import * as sass from 'gulp-ruby-sass';
 import * as project from '../aurelia.json';
 import {build} from 'aurelia-cli';
 
 export default function processCSS() {
-  return gulp.src(project.cssProcessor.source)
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest(project.paths.root))
+  return sass(project.cssProcessor.source, {sourcemap: true})
+    .on('error', sass.logError)
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('src'))
     .pipe(build.bundle());
 };
